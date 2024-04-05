8c25ceeb69ca7c23c4ee839b79412d5b6491b4b1
Salvatore Bono <salvatorebono2001@gmail.com>
2024-04-05 14:59:08 +0200

    rename class

--- src/org/list/WishList.java
+++ src/org/list/WishList.java
@@ -3,7 +3,7 @@ package org.list;
 import java.io.File;
 import java.util.*;
 
-public class Main {
+public class WishList {
     public static void main(String[] args) {
         File root = new File("./resources/wish-list.txt");
         Scanner scan = new Scanner(System.in);
