a5e5542a87bef0ad2f60488c6d431f902ae81550
Salvatore Bono <salvatorebono2001@gmail.com>
2024-04-05 14:50:04 +0200

    start bonus

---
+++ src/org/list/Gift.java
@@ -0,0 +1,13 @@
+package org.list;
+
+public class Gift {
+    String name;
+
+    public Gift(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/org/list/Main.java
@@ -0,0 +1,40 @@
+package org.list;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File root = new File("./resources/wish-list.txt");
+        Scanner scan = new Scanner(System.in);
+        List<String> gifts = new ArrayList<>();
+        boolean exit = false;
+        do {
+            System.out.println("Which gift you would like to add to your wish list:");
+            String gift = scan.nextLine();
+
+            //aggiungi gift all'array gifts
+            gifts.add(gift);
+
+            //ogni iterazione deve mostrare la lunghezza della lista
+            System.out.println("On your wish list are " + gifts.size() + " gifts");
+
+            System.out.println("would you like to add more gifts? Y/N");
+            String addGiftOrNo = scan.nextLine().toUpperCase();
+            if (Objects.equals(addGiftOrNo, "N")){
+                System.out.println("Your wish list has been updated");
+                exit = true;
+            }
+        } while (!exit);
+
+        System.out.println("On your wish list are:");
+        for (String singleGift : gifts){
+            System.out.println("- "+singleGift);
+        }
+
+        scan.close();
+    }
+}
